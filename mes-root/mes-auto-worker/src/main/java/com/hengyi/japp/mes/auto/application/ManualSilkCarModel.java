c7f28e5029dbe84c11a901592e3d1a0a9222b881
jzb <ixtf1984@gmail.com>
2019-03-28 00:58:00 +0800

    fix c auto doffing

--- mes-root/mes-auto-worker/src/main/java/com/hengyi/japp/mes/auto/application/ManualSilkCarModel.java
+++ mes-root/mes-auto-worker/src/main/java/com/hengyi/japp/mes/auto/application/ManualSilkCarModel.java
@@ -128,8 +128,8 @@ public class ManualSilkCarModel extends AbstractSilkCarModel {
             final int size = silkBarcodes.size();
             if (builder.build().size() == 10 && size == 1) {
                 final List<Single<SilkRuntime>> collect = generateSilkRuntimesBySilkBarcodesC(SilkCarSideType.B, silkBarcodes.get(1)).collect(toList());
-                builder.addAll(collect);
-                return Single.merge(builder.build()).toList();
+                collect.addAll(builder.build());
+                return Single.merge(collect).toList();
             }
         }
         throw new RuntimeException("验证数不符!");
