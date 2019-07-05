7c6d40bc33bf96c54dc32ccc869f199d2467ddd0
liuyuan <hi60@qq.com>
2019-07-05 20:56:22 +0800

    add:board

---
+++ client/board/js/board-global.js
@@ -0,0 +1,15 @@
+window.global = {
+   //ajaxAutomatictBaseUrl: 'http://192.168.1.128:9999/',
+   //ajaxAutomatictBaseUrl: 'http://10.61.0.15:9999/',
+    //ajaxAutomatictBaseUrl: 'http://10.12.9.0:9999/',
+    ajaxAutomatictBaseUrl:'http://10.61.0.15:9999/',
+    //ajaxAutomatictBaseUrl: 'http://' + window.document.location.hostname + ':9999/'
+    companyName: '杭州化纤逸暻有限公司',
+    paramName_A : ['A1','A2','A3','A4','A5','A6','A7','A8'],
+    paramName_B : ['B1','B2'],
+    paramName_C : ['C1','C2','C3','C4','C5','C6'],
+    paramName_D : ['D1','D2','D3','D4','D5','D6'],
+    paramName_ALL:['A1','A2','A3','A4','A5','A6','A7','A8','B1','B2','C1','C2','C3','C4','C5','C6','D1','D2','D3','D4','D5','D6'],
+    num:3
+
+}
