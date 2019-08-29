e595a4a437a310f6aa8a6f57583541c978ee348e
jzb <ixtf1984@gmail.com>
2019-08-29 14:43:26 +0800

    fix auto line display

--- mes-root/mes-auto-agent/src/main/java/com/hengyi/japp/mes/auto/agent/verticle/PdaVerticle.java
+++ mes-root/mes-auto-agent/src/main/java/com/hengyi/japp/mes/auto/agent/verticle/PdaVerticle.java
@@ -1,5 +1,6 @@
 package com.hengyi.japp.mes.auto.agent.verticle;
 
+import com.github.ixtf.japp.core.J;
 import com.github.ixtf.japp.vertx.Jvertx;
 import com.hengyi.japp.mes.auto.config.MesAutoConfig;
 import io.reactivex.Completable;
@@ -7,7 +8,11 @@ import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.MultiMap;
+import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.core.eventbus.Message;
+import io.vertx.reactivex.core.http.HttpServerResponse;
+import io.vertx.reactivex.ext.auth.User;
 import io.vertx.reactivex.ext.auth.jwt.JWTAuth;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
@@ -15,9 +20,15 @@ import io.vertx.reactivex.ext.web.handler.JWTAuthHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
 import static com.hengyi.japp.mes.auto.agent.Agent.INJECTOR;
+import static java.util.stream.Collectors.toMap;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM;
 
 /**
  * @author jzb 2018-06-20
@@ -42,20 +53,22 @@ public class PdaVerticle extends AbstractVerticle {
         router.route("/api/*").handler(JWTAuthHandler.create(jwtAuth));
 
         router.post("/statisticReport/generate").produces(APPLICATION_JSON)
-                .handler(rc -> common(rc, "mes-auto:report:statisticReport:generate"));
+                .handler(rc -> commonSend(rc, "mes-auto:report:statisticReport:generate"));
         router.post("/statisticReport/fromDisk").produces(APPLICATION_JSON)
-                .handler(rc -> common(rc, "mes-auto:report:statisticReport:fromDisk"));
+                .handler(rc -> commonSend(rc, "mes-auto:report:statisticReport:fromDisk"));
         router.post("/statisticReport/rangeDisk").produces(APPLICATION_JSON)
-                .handler(rc -> common(rc, "mes-auto:report:statisticReport:rangeDisk"));
+                .handler(rc -> commonSend(rc, "mes-auto:report:statisticReport:rangeDisk"));
+        router.post("/statisticReport/download").produces(APPLICATION_OCTET_STREAM)
+                .handler(rc -> commonSend(rc, "mes-auto:report:statisticReport:download"));
 
         router.post("/dyeingReport").produces(APPLICATION_JSON)
-                .handler(rc -> common(rc, "mes-auto:report:dyeingReport", setMinutes(5)));
+                .handler(rc -> commonSend(rc, "mes-auto:report:dyeingReport", Duration.ofMinutes(5)));
         router.post("/strippingReport").produces(APPLICATION_JSON)
-                .handler(rc -> common(rc, "mes-auto:report:strippingReport", setMinutes(10)));
+                .handler(rc -> commonSend(rc, "mes-auto:report:strippingReport", Duration.ofMinutes(10)));
         router.post("/measureFiberReport").produces(APPLICATION_JSON)
-                .handler(rc -> common(rc, "mes-auto:report:measureFiberReport", setMinutes(5)));
+                .handler(rc -> commonSend(rc, "mes-auto:report:measureFiberReport", Duration.ofMinutes(5)));
         router.post("/silkExceptionReport").produces(APPLICATION_JSON)
-                .handler(rc -> common(rc, "mes-auto:report:silkExceptionReport", setMinutes(3)));
+                .handler(rc -> commonSend(rc, "mes-auto:report:silkExceptionReport", Duration.ofMinutes(5)));
 
 
         router.get("/api/reports/doffingSilkCarRecordReport").produces(APPLICATION_JSON).handler(rc -> {
@@ -89,20 +102,49 @@ public class PdaVerticle extends AbstractVerticle {
                 .ignoreElement();
     }
 
-    private DeliveryOptions setMinutes(long minutes) {
-        return new DeliveryOptions().setSendTimeout(Duration.ofMinutes(minutes).toMillis());
+    private void commonSend(RoutingContext rc, String address) {
+        commonSend(rc, address, new DeliveryOptions());
     }
 
-    private void common(RoutingContext rc, String address) {
-        DeliveryOptions deliveryOptions = new DeliveryOptions().setSendTimeout(Duration.ofMinutes(1).toMillis());
-        common(rc, address, deliveryOptions);
+    private void commonSend(RoutingContext rc, String address, Duration duration) {
+        final DeliveryOptions deliveryOptions = new DeliveryOptions().setSendTimeout(duration.toMillis());
+        commonSend(rc, address, deliveryOptions);
     }
 
-    private void common(RoutingContext rc, String address, DeliveryOptions deliveryOptions) {
-        vertx.eventBus().rxSend(address, rc.getBodyAsString(), deliveryOptions).subscribe(reply -> {
-            String ret = (String) reply.body();
-            rc.response().end(ret);
+    private void commonSend(RoutingContext rc, String address, DeliveryOptions deliveryOptions) {
+        vertx.eventBus().rxSend(address, encode(rc), deliveryOptions).subscribe(reply -> {
+            final HttpServerResponse response = rc.response();
+            final MultiMap headers = reply.headers();
+            headers.entries().forEach(it -> response.putHeader(it.getKey(), it.getValue()));
+            response.end(buffer(reply));
         }, rc::fail);
+    }
+
+    private JsonObject encode(RoutingContext rc) {
+        final JsonObject principal = Optional.ofNullable(rc.user()).map(User::principal).orElse(null);
+        final Map<String, String> pathParams = rc.pathParams();
+        final Map<String, List<String>> queryParams = rc.queryParams().names().parallelStream().collect(toMap(Function.identity(), rc.queryParams()::getAll));
+        return new JsonObject().put("principal", principal)
+                .put("pathParams", pathParams)
+                .put("queryParams", queryParams)
+                .put("body", rc.getBodyAsString());
+    }
+
+    private Buffer buffer(Message<Object> reply) {
+        final Object body = reply.body();
+        if (body == null) {
+            return Buffer.buffer();
+        }
+        if (body instanceof String) {
+            final String result = (String) body;
+            if (J.isBlank(result)) {
+                return Buffer.buffer();
+            } else {
+                return Buffer.buffer(result);
+            }
+        }
+        final byte[] bytes = (byte[]) body;
+        return Buffer.buffer(bytes);
     }
 
 }
